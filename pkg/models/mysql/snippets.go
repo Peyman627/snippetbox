78b8b3667f0263cfe554fa385f010beea49b92eb
peyman <hassani.peyman627@gmail.com>
2022-08-18 10:43:33 +0430

    Update `Insert` method on `SnippetModel`

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -3,7 +3,7 @@ package mysql
 import (
 	"database/sql"
 
-	"snippetbox/pkg/models"
+	"github.com/Peyman627/snippetbox/pkg/models"
 )
 
 type SnippetModel struct {
@@ -12,7 +12,16 @@ type SnippetModel struct {
 
 // Insert will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created, expires) 
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	return int(id), nil
 }
 
 // Get will return a specific snippet based on its id.
